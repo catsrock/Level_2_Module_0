291a86675d72595489950c38ab50e06da4b65815
catsrock <league@WTS-IM-17.attlocal.net>
2017-11-05 14:30:39 -0800

    Learned about ArrayLists
    Also learned about foreach
    Code was more difficult today, but lots of fun!!!

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,39 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	static JFrame frame=new JFrame();
+	static JPanel panel=new JPanel();
+	static JButton butt1=new JButton();
+	static JButton butt2=new JButton();
+	static ArrayList<String> names=new ArrayList<String>();
+	public static void main(String[] args) {
+		GuestBook g1=new GuestBook();
+		
+		
+	}
+	public GuestBook() {
+		createUI();
+	}
+	public void createUI() {
+		frame.setVisible(true);
+		frame.add(panel);
+		butt1.setText("Add Name");
+		butt1.addActionListener(this);
+		panel.add(butt1);
+		butt2.setText("View Names");
+		butt2.addActionListener(this);
+		panel.add(butt2);
+		frame.pack();
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +42,24 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		String output="";
+		if(e.getSource()==butt1) {
+			String name=JOptionPane.showInputDialog("Enter your name");
+			
+			names.add(name);
+		}
+		
+		else if(e.getSource()==butt2) {
+			int i=1;
+			for (String d : names) {
+				
+				output+=("Guest # "+ i++ +":"+" "+d+"\n");
+			}
+			JOptionPane.showMessageDialog(null, output);
+		}
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,44 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
+		ArrayList<String> s=new ArrayList<String>();
 		//   Don't forget to import the ArrayList class
-		
+		s.add("potato");
+		s.add("tomato");
+		s.add("thing 1");
+		s.add("thing 2");
+		s.add("the fifth object");
 		//2. Add five Strings to your list
 		
 		//3. Print all the Strings using a standard for-loop
-		
+	//	for (int i = 0; i < s.size(); i++) {
+		//	System.out.println(s.get(i));
+		//}
 		//4. Print all the Strings using a for-each loop
-		
+		//for (String f : s) {
+		//	System.out.println(f);
+		//}
 		//5. Print only the even numbered elements in the list.
-		
+	//	for (int i = 0; i < s.size(); i++) {
+		//	if(i%2==0) {
+		//	System.out.println(s.get(i));
+			//}
+	
+		//	for (int i = s.size()-1; i >= 0 ; i--) {
+			//System.out.println(s.get(i));
+	//	}
+			for (String f : s) {
+				if(f.contains("e")) {
+				System.out.println(f);
+			}
+		}
 		//6. Print all the Strings in reverse order.
 		
 		//7. Print only the Strings that have the letter 'e' in them.
 	}
 }
+
